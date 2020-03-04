76175852f2a660243e4003db7b17a5e44f87a21b
Max2411 <maxvankemenade@gmail.com>
2020-03-04 20:41:10 +0100

    fix funcite

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import psycopg2
 
 #postgres
-conn = psycopg2.connect(database="voordeelshop", user = "postgres", password = "groep5", host = "127.0.0.1", port = "5432")
+conn = psycopg2.connect(database="voordeelshop", user = "postgres", password = "kruimeltje14", host = "127.0.0.1", port = "5432")
 cur = conn.cursor()
 print("Opened database successfully")
 
@@ -25,18 +25,22 @@ def overzetten_products():
     print(products[0]['_id'])
     i=0
     for x in products:
-        if x['_id'] in product_id:
-            i+=1
-        else:
-            productid = x['_id']
-            brand= x['brand']
-            category = x['category']
-            gender = x['gender']
-            doelgroep = x['properties']['doelgroep']
-            price= x['price']['selling_price']
-            price=price/100
-            cur.execute("insert into products (product_id, brand, category, gender,doelgroep,price) values (%s,%s,%s,%s,%s,%s)",(productid, brand, category, gender,doelgroep,price))
-            i+=1
+        try:
+            if x['_id'] in product_id:
+                i+=1
+            else:
+                productid = x['_id']
+                brand= x['brand']
+                category = x['category']
+                gender = x['gender']
+                doelgroep = x['properties']['doelgroep']
+                price= x['price']['selling_price']
+                price=price/100
+                cur.execute("insert into products (product_id, brand, category, gender,doelgroep,price) values (%s,%s,%s,%s,%s,%s)",(productid, brand, category, gender,doelgroep,price))
+                i+=1
+        except KeyError:
+            continue
+
     conn.commit()
     cur.close()
     conn.close()
@@ -44,13 +48,4 @@ def overzetten_products():
 
 
 overzetten_products()
-cur.execute('select product_id from products')
-product_id = cur.fetchall()
-print(sessions[0])
-print(products[0]["_id"])
 
-#for x in products:
-#   print(x['brand'])
-
-cur.close()
-conn.close()
