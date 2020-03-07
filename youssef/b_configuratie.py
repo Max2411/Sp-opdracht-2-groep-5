497b6a12d635f75ecc00a698469375e44c4fbe0e
y0urselff <52598379+y0urselff@users.noreply.github.com>
2020-03-07 13:12:15 +0100

    Update and rename c_configuratie.py to b_configuratie.py

--- youssef/b_configuratie.py
+++ youssef/b_configuratie.py
@@ -2,7 +2,7 @@
 from configparser import ConfigParser
 
 
-def config(filename='verbinding_parameters.ini', section='postgresql'):
+def config(filename='a_verbinding_parameters.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
