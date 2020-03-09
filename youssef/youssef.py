e589c61ffb07926a55249d3a708cc250ff271ae7
y0urselff <52598379+y0urselff@users.noreply.github.com>
2020-03-09 13:01:44 +0100

    Update youssef.py

--- youssef/youssef.py
+++ youssef/youssef.py
@@ -10,7 +10,7 @@ client = MongoClient('mongodb://localhost:27017/')
 # cursor = connection.cursor()
 client = MongoClient('mongodb://localhost:27017/')
 database = client.huwebshop
-# .x aanpassen om andere collection op te halen
+# voor onderstaande:.x aanpassen om andere collectie op te halen
 ophalen = database.products
 session = database.sesssions
 products = ophalen.find({})
@@ -21,31 +21,20 @@ sessions = session.find({})
 #     print(item)
 
 # specifieke object(en) uit collection ophalen
-# for x in ophalen.find({}, {"_id":1}):
-# #     print(x)
+mydict = []
+for specifiek_object in ophalen.find({}, {"_id":1}):
+    formateren = specifiek_object
+    mydict.append(formateren)
 
-# naar csv schrijven
-# my data rows as dictionary objects
-mydict =[{'branch': 'COE', 'cgpa': '9.0', 'name': 'Nikhil', 'year': '2'},
-         {'branch': 'COE', 'cgpa': '9.1', 'name': 'Sanchit', 'year': '2'},
-         {'branch': 'IT', 'cgpa': '9.3', 'name': 'Aditya', 'year': '2'},
-         {'branch': 'SE', 'cgpa': '9.5', 'name': 'Sagar', 'year': '1'},
-         {'branch': 'MCE', 'cgpa': '7.8', 'name': 'Prateek', 'year': '3'},
-         {'branch': 'EP', 'cgpa': '9.1', 'name': 'Sahil', 'year': '2'}]
+print(mydict)
 
-# field names
-fields = ['name', 'branch', 'year', 'cgpa']
-
-# name of csv file
-filename = "products.csv"
-
-# writing to csv file
-with open(filename, 'w') as csvfile:
+# schrijven naar csv bestand
+veldnaam = ['_id']
+bestandsnaam = 'test.csv'
+with open(bestandsnaam, 'w') as csvfile:
     # creating a csv dict writer object
-    writer = csv.DictWriter(csvfile, fieldnames=fields)
-
-    # writing headers (field names)
+    writer = csv.DictWriter(csvfile, fieldnames=veldnaam, lineterminator='\n')
+    # writing headers (veldnaam)
     writer.writeheader()
-
     # writing data rows
     writer.writerows(mydict)
