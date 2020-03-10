3d5570e5bb2fc8f945288e5f8375680b542c0ee6
Max2411 <maxvankemenade@gmail.com>
2020-03-10 14:43:33 +0100

    connection file

--- db_setup.py
+++ db_setup.py
@@ -5,8 +5,12 @@ conn= psycopg2.connect('dbname=postgres user=postgres password=groep5')
 #conn = psycopg2.connect(database="voordeelshop", user = "postgres", password = "groep5", host = "127.0.0.1", port = "5432")
 cur = conn.cursor()
 print("Opened database successfully")
+cur.execute("DROP TABLE IF EXISTS order_table ")
 cur.execute("DROP TABLE IF EXISTS products ")
 cur.execute("DROP TABLE IF EXISTS sessions ")
+cur.execute("DROP TABLE IF EXISTS brand ")
+cur.execute("DROP TABLE IF EXISTS profile ")
+cur.execute("DROP TABLE IF EXISTS category ")
 cur.execute("""create table products
             (product_id varchar PRIMARY KEY,
 	        brand varchar,
@@ -14,11 +18,27 @@ cur.execute("""create table products
 	        gender varchar,
 	        doelgroep varchar,
 	        price decimal(6,2));""")
-#cur.execute("""create table sesions
-#            (session_id varchar PRIMARY KEY,
-#
-#);
-#""")
+cur.execute("""create table sessions
+           (session_id varchar PRIMARY KEY           );""")
+cur.execute("""CREATE TABLE order_table
+           (order_id serial primary key,
+           product_id varchar,
+           session_id varchar);""")
+cur.execute("""create table brand
+           (brand_id serial primary key,
+           brand varchar unique
+           );""")
+cur.execute("""create table category
+           (category_id varchar PRIMARY KEY,
+           category varchar
+           );""")
+cur.execute("""alter table order_table
+            add constraint Prod_id
+            foreign key(product_id) references products(product_id);""")
+
+# cur.execute("""create table category
+#            (category_id varchar PRIMARY KEY,
+#            );""")
 
 
 
