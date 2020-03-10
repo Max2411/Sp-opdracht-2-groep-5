7b197d5b96cf23ab5350d91421fab777c93c2a45
Max2411 <maxvankemenade@gmail.com>
2020-03-10 14:44:00 +0100

    update

--- max/Test.py
+++ max/Test.py
@@ -1,21 +1,10 @@
-from pymongo import MongoClient
-import psycopg2
 import csv
-conn= psycopg2.connect('dbname=postgres user=postgres password=groep5')
+from Connections import psycopg_connect
+from Connections import mongo_connect
 
-#postgres
-# conn = psycopg2.connect(database="voordeelshop", user = "postgres", password = "groep5", host = "127.0.0.1", port = "5432")
-cur = conn.cursor()
-print("Opened database successfully")
+conn,cur=psycopg_connect()
 
-#mongodb
-client = MongoClient('mongodb://localhost:27017/')
-db = client.huwebshop
-col = db.products
-ses= db.sessions
-products = col.find({})
-sessions= ses.find({})
-
+products, sessions, profiles=mongo_connect()
 
 def overzetten_products(): #bron: slack info van de les gestuurd door rik boss
     with open('products.csv', 'w', newline='') as csvout:
