4d6004a33ebd2fd17671418448cba94099a6b226
Max2411 <maxvankemenade@gmail.com>
2020-03-11 15:17:31 +0100

    database update

--- db_setup.py
+++ db_setup.py
@@ -13,12 +13,12 @@ cur.execute("DROP TABLE IF EXISTS products ")
 
 cur.execute("""create table products
             (product_id varchar PRIMARY KEY,
-	        brand varchar,
-	        category varchar,
+	        brand integer,
+	        category integer,
 	        sub_category varchar,
 	        sub_sub_category varchar,
 	        gender varchar,
-	        target_audience varchar, # doelgroep veranderen naar target audience
+	        target_audience varchar, 
 	        price decimal(6,2));""")
 cur.execute("""create table sessions
            (session_id varchar PRIMARY KEY           );""")
@@ -32,7 +32,7 @@ cur.execute("""create table brand
            );""")
 cur.execute("""create table category
            (category_id serial PRIMARY KEY,
-           sub_category_id serial
+           sub_category_id serial,
            category varchar
            );""")
 cur.execute("""create table sub_category
@@ -46,23 +46,23 @@ cur.execute("""create table sub_sub_category
            sub_sub_category varchar
            );""")
 
-cur.execute("""alter table category
+cur.execute("""alter table products
             add constraint cat_id
-            foreign key(category_id) references products(category);""")
-cur.execute("""alter table sub_category
-            add constraint subcat_id
-            foreign key(sub_category_id) references category(sub_category_id);""")
-cur.execute("""alter table sub_category
-            add constraint subcat_id2
-            foreign key(sub_category_id) references products(sub_category);""")
-
-cur.execute("""alter table sub_sub_category
-            add constraint subsubcat_id
-            foreign key(sub_sub_category_id) references sub_category(sub_sub_category_id);""")
-
-cur.execute("""alter table sub_sub_category
-            add constraint subsubcat_id2
-            foreign key(sub_sub_category_id) references products(sub_sub_category);""")
+            foreign key(category) references category(category_id);""")
+# cur.execute("""alter table sub_category
+#             add constraint subcat_id
+#             foreign key(sub_category_id) references category(sub_category_id);""")
+# cur.execute("""alter table sub_category
+#             add constraint subcat_id2
+#             foreign key(sub_category_id) references products(sub_category);""")
+#
+# cur.execute("""alter table sub_sub_category
+#             add constraint subsubcat_id
+#             foreign key(sub_sub_category_id) references sub_category(sub_sub_category_id);""")
+#
+# cur.execute("""alter table sub_sub_category
+#             add constraint subsubcat_id2
+#             foreign key(sub_sub_category_id) references products(sub_sub_category);""")
 
 cur.execute("""alter table order_table
             add constraint Prod_id
@@ -70,9 +70,9 @@ cur.execute("""alter table order_table
 cur.execute("""alter table order_table
             add constraint Ses_id
             foreign key(session_id) references sessions(session_id);""")
-cur.execute("""alter table brand
+cur.execute("""alter table products
             add constraint brand_id
-            foreign key(brand_id) references products(brand);""")
+            foreign key(brand) references brand(brand_id);""")
 
 
 
